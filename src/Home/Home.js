7e2e17b382891d1c043f15f5730a7d6f7398608d
407 <407@academicos.cesde.edu.co>
2022-11-29 16:40:41 -0500

    react

---
+++ src/Home/Home.js
@@ -0,0 +1,88 @@
+import "./Home.css"
+
+export function Home(){
+
+return(
+<>
+
+<section>
+    
+<div class="banner"></div>
+
+    </section> 
+    <section>
+
+<div class="container">
+    <div class="row">
+
+    <div class="col-12 col-md-6">
+        <h2>Gorillax</h2>
+        <p>Gorillaz es una banda virtual inglesa creada en 1998 por Damon Albarn y Jamie Hewlett. La banda está compuesta por cuatro miembros ficticios: 2-D, Noodle, Murdoc Niccals y Russel Hobbs. La mayoría de sus canciones junto con todo su universo ficticio, se presentan a través de vídeos musicales animados, de animación tradicional y animación por computadora, entrevistas y pequeños cortos animados. En la realidad, Albarn es el único miembro permanente de la banda. El productor y ejecutivo Remi Kabaka Jr. empezaría a colaborar con el grupo en 2016 después de proveer por varios años la voz de Russel Hobbs y sería nombrado miembro oficial junto a Albarn y Hewlett en 2019 en el documental de Gorillaz, Gorillaz: Reject False Icons.1​</p>
+    </div>
+    <div class="col-12 col-md-6">
+    <i class="bi bi-vinyl-fill fs-1"><h1 class="letra"> Medellin</h1> <p class="letra" >Dec2</p>  </i>
+    <i class="bi bi-vinyl-fill fs-1">  <h1 class="letra"> Medellin</h1> <p class="letra" >Dec3</p> </i>
+    <i class="bi bi-vinyl-fill fs-1"> <h1 class="letra"> Medellin</h1> <p class="letra" >Dec5</p></i>
+    <i class="bi bi-vinyl-fill fs-1"> <h1 class="letra"> Medellin</h1> <p class="letra" >Dec5</p></i>
+    </div>
+
+
+
+    </div>
+</div>
+
+    </section>
+
+
+
+
+
+<section>
+
+        <div class="container-fluid">
+
+            <div class="row">
+                        <div class="col-12 col-md-4 " >
+
+
+                                <img src="https://firebasestorage.googleapis.com/v0/b/spotifay-ddec4.appspot.com/o/noticia1.jpg?alt=media&token=a80ae65c-6c03-4bcb-b789-304450a4ea55" alt=""  class="img-fluid w-100 carouselExampleControls" />
+
+
+
+
+
+                        </div>
+                        <div class="col-12 col-md-4 " >
+
+
+<img src="https://firebasestorage.googleapis.com/v0/b/spotifay-ddec4.appspot.com/o/noticia2.jpg?alt=media&token=e24ab6b0-6dc7-49a8-9c28-7fdc980c5217" />
+
+
+
+
+
+</div>
+<div class="col-12 col-md-4 " >
+
+
+<img src="https://firebasestorage.googleapis.com/v0/b/spotifay-ddec4.appspot.com/o/noticia3.jpg?alt=media&token=ed33b842-b44b-4471-ad9a-5fecab83667f" alt=""  class="img-fluid w-100 carouselExampleControls" />
+
+
+
+
+
+</div>
+            </div>
+
+        </div>
+
+</section>
+
+
+
+
+</>
+)
+
+}
+
